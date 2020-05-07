89b0b2080740dc62278ac6c131154aa2e49c1d27
amine <aminenichan@amines-MacBook-Pro.local>
2020-05-07 19:07:50 +0000

    admin dashboard add

--- gestioninterventionpfa/settings.py
+++ gestioninterventionpfa/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'admin',
+    'admins',
     'technicien',
 ]
 
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
