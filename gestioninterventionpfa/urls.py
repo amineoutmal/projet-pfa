a2dcde703f013a5e8bb5597a5fab0fe38bc355d0
AmehZak <amehzak@gmail.com>
2020-05-07 20:44:49 +0000

    amin-commit

--- gestioninterventionpfa/urls.py
+++ gestioninterventionpfa/urls.py
@@ -2,12 +2,9 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-<<<<<<< HEAD
     path('admins/',include("admins.urls")),
 
-=======
     path('admin/', admin.site.urls),
     path('admin/', admin.site.urls),
     path('admin/', admin.site.urls),
->>>>>>> d2b816c9861db44a93e1722ed3d19c36d814f10c
 ]
